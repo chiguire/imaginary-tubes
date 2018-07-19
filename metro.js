905ca137fe91f7964660dca910f00b54e0aea65d
Ciro Durán <ciro.duran@gmail.com>
2018-07-19 13:37:24 +0100

    Wonky red line

--- metro.js
+++ metro.js
@@ -1,14 +1,16 @@
 const path = require('path'),
       atob = require('atob'),
       {fabric} = require('fabric'),
-      fs = require('fs');
+      fs = require('fs'),
+      MersenneTwister = require('mersenne-twister');
 
-function gridDescription(originX, originY, tileWidth, tileHeight, canvasOriginX, canvasOriginY, canvasWidth, canvasHeight) {
+function gridDescription(originX, originY, tileWidth, tileHeight, canvasOriginX, canvasOriginY, canvasWidth, canvasHeight, randomGenerator) {
   const gridDescBasic = {
     originX,
     originY,
     tileWidth,
     tileHeight,
+    rnd: randomGenerator
   };
   const gridDescDims = {
     tl: gridPointFromWorldSpace(
@@ -157,14 +159,14 @@ function edgesFromPoint(tX,tY,gridDesc) {
 function drawTetrakisGrid(c, gridDesc) {
   const points = allVertices(gridDesc);
   const edges = allEdges(gridDesc);
-  const newEdges = [].concat(
-    edgesFromPoint(gridDesc.tl.tX, gridDesc.tl.tY, gridDesc),
-    edgesFromPoint(gridDesc.tl.tX, gridDesc.br.tY, gridDesc),
-    edgesFromPoint(gridDesc.br.tX, gridDesc.tl.tY, gridDesc),
-    edgesFromPoint(gridDesc.br.tX, gridDesc.br.tY, gridDesc),
-    edgesFromPoint(2, 2, gridDesc),
-    edgesFromPoint(3, 4, gridDesc)
-  );
+  // const newEdges = [].concat(
+    // edgesFromPoint(gridDesc.tl.tX, gridDesc.tl.tY, gridDesc),
+    // edgesFromPoint(gridDesc.tl.tX, gridDesc.br.tY, gridDesc),
+    // edgesFromPoint(gridDesc.br.tX, gridDesc.tl.tY, gridDesc),
+    // edgesFromPoint(gridDesc.br.tX, gridDesc.br.tY, gridDesc),
+    // edgesFromPoint(2, 2, gridDesc),
+    // edgesFromPoint(3, 4, gridDesc)
+  // );
   edges.forEach(function (edge) {
     var e = new fabric.Line([edge.a.x,edge.a.y,edge.b.x,edge.b.y], {
       stroke: 'rgba(3,3,3,0.33)',
@@ -183,12 +185,132 @@ function drawTetrakisGrid(c, gridDesc) {
     c.add(p);
   });
   
-  newEdges.forEach(function (edge) {
-    var e = new fabric.Line([edge.a.x,edge.a.y,edge.b.x,edge.b.y], {
-      stroke: 'rgba(255,0,0,1)',
-      strokeWidth: 1
+  // newEdges.forEach(function (edge) {
+    // var e = new fabric.Line([edge.a.x,edge.a.y,edge.b.x,edge.b.y], {
+      // stroke: 'rgba(255,0,0,1)',
+      // strokeWidth: 1
+    // });
+    // c.add(e);
+  // });
+}
+
+function edgeExtremes() {
+  return ['lr','rl','ud','du'];
+}
+
+function choose(arr, rndGen) {
+  return arr[Math.floor(rndGen.random() * arr.length)];
+}
+
+function vertexInsideGrid(tX, tY, gridDesc) {
+  return gridDesc.tl.tX <= tX &&
+         tX <= gridDesc.br.tX &&
+         gridDesc.tl.tY <= tY &&
+         tY <= gridDesc.br.tY;
+}
+
+function chooseDirection(dX, dY, extremes, rnd) {
+  if (dX < 0) {
+    var nX = Math.round(rnd.random()-1); // -1 or 0
+  } else if (dX > 0) {
+    var nX = Math.round(rnd.random()); // 0 or 1
+  } else {
+    var nX = Math.round((rnd.random() - 0.5)*2.9);
+  }
+  
+  if (dY < 0) {
+    var nY = Math.round(rnd.random()-1); // -1 or 0
+  } else if (dY > 0) {
+    var nY = Math.round(rnd.random()); // 0 or 1
+  } else {
+    var nY = Math.round((rnd.random() - 0.5)*2.9);
+  }
+  
+  return { dX: nX, dY: nY };
+}
+
+function generateSegments(extremes, gridDesc) {
+  var rnd = gridDesc.rnd;
+  switch (extremes) {
+    case 'lr':
+      var pX = 0;
+      var pY = Math.floor(gridDesc.tl.tY+rnd.random()*(gridDesc.br.tY - gridDesc.tl.tY));
+      var dX = 1;
+      var dY = Math.round((rnd.random()-0.5)*2); // bias towards not moving
+      break;
+    case 'rl':
+      var pX = gridDesc.br.tX;
+      var pY = Math.floor(gridDesc.tl.tY+rnd.random()*(gridDesc.br.tY - gridDesc.tl.tY));
+      var dX = -1;
+      var dY = Math.round((rnd.random()-0.5)*2); // bias towards not moving
+      break;
+    case 'ud':
+      var pX = Math.floor(gridDesc.tl.tX+rnd.random()*(gridDesc.br.tX - gridDesc.tl.tX));
+      var pY = 0;
+      var dX = Math.round((rnd.random()-0.5)*2); // bias towards not moving
+      var dY = 1;
+      break;
+    case 'du':
+      var pX = Math.floor(gridDesc.tl.tX+rnd.random()*(gridDesc.br.tX - gridDesc.tl.tX));
+      var pY = gridDesc.br.tY;
+      var dX = Math.round((rnd.random()-0.5)*2); // bias towards not moving
+      var dY = -1;
+      break;
+    default:
+      console.log('UNKNOWN EXTREMES ' + extremes);
+      break;
+  }
+  
+  var segments = [];
+  var vertices = [{x: pX, y: pY}];
+  const givenSegmentDistance = 5;
+  var currentSegmentDistance = givenSegmentDistance;
+  while (vertexInsideGrid(pX+dX, pY+dY, gridDesc)) {
+    const newVertex = {x: pX+dX, y: pY+dY};
+    const newEdge = edgeBetweenGridPoints(pX, pY, newVertex.x, newVertex.y, gridDesc);
+    segments.push(newEdge);
+    vertices.push(newVertex);
+    
+    pX += dX;
+    pY += dY;
+    currentSegmentDistance -= 1;
+    if (currentSegmentDistance === 0) {
+      var newDirection = chooseDirection(dX, dY, extremes, rnd);
+      dX = newDirection.dX;
+      dY = newDirection.dY;
+      currentSegmentDistance = givenSegmentDistance;
+    }
+  }
+  return { segments, vertices };
+}
+
+function tubeLine(name, color, otherLines, gridDesc) {
+  const extremes = choose(edgeExtremes(), gridDesc.rnd);
+  const { segments, vertices } = generateSegments(extremes, gridDesc);
+  return {
+    name,
+    color,
+    extremes,
+    segments, //layoutSegments
+  };
+}
+
+function tubeLines(gridDesc) {
+  return [
+    tubeLine('Central', 'rgb(255,0,0)', [], gridDesc),
+  ];
+}
+
+function drawLines(c, lines) {
+  lines.forEach(function (line) {
+    console.log(line.extremes);
+    line.segments.forEach(function (edge) {
+      var e = new fabric.Line([edge.a.x,edge.a.y,edge.b.x,edge.b.y], {
+        stroke: 'rgba(255,0,0,1)',
+        strokeWidth: 1
+      });
+      c.add(e);
     });
-    c.add(e);
   });
 }
 
@@ -201,11 +323,15 @@ function choumein() {
       backgroundColor: '#ffffff'
     }
   );
+  const rnd = new MersenneTwister(Date.now().valueOf());
   fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
   
-  const gridDesc = gridDescription(0, 0, 20, 10, 0, 0, canvas.width-20, canvas.height-10);
+  const gridDesc = gridDescription(0, 0, 20, 10, 0, 0, canvas.width-20, canvas.height-10, rnd);
   //console.log(gridDesc);
+  const lines = tubeLines(gridDesc);
+  
   drawTetrakisGrid(canvas, gridDesc);
+  drawLines(canvas, lines);
   
   const pngImageB64 = canvas.toDataURL({ format: 'png' });
   const pngImage = atob(pngImageB64.split(',')[1]);
